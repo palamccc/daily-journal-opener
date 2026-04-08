02de52fbb562bc7e682a6c5ab5a49dc22bc8e2f6
palani <palam.c@gmail.com>
2026-04-08 22:40:27 +1000

    .

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,25 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { mkdir, writeFile } from 'node:fs/promises';
+import { join } from 'node:path';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "daily-journal-opener" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('daily-journal-opener.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from daily-journal-opener!');
-	});
-
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(vscode.commands.registerCommand('daily-journal-opener.openToday', async () => {
+		const folder = vscode.workspace.workspaceFolders?.[0];
+		if (!folder) {
+			await vscode.window.showErrorMessage('Open a folder first');
+			return;
+		}
+		const now = new Date();
+		const yyyy = String(now.getFullYear());
+		const mm = String(now.getMonth() + 1).padStart(2, '0');
+		const dd = String(now.getDate()).padStart(2, '0');
+		const dir = join(folder.uri.fsPath, 'journal', `${yyyy}-${mm}`);
+		const file = join(dir, `${yyyy}-${mm}-${dd}.md`);
+		await mkdir(dir, { recursive: true });
+		await writeFile(file, '', { flag: 'a' });
+		const doc = await vscode.workspace.openTextDocument(file);
+		await vscode.window.showTextDocument(doc);
+	}));
 }
 
-// This method is called when your extension is deactivated
 export function deactivate() {}
